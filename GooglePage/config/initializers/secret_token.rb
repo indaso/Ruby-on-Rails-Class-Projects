a094e8949f76cbf6fd540b0047308a0e0defb051
indaso <indaso5@gmail.com>
2014-01-29 22:17:29 -0500

    Basic HTML presentation

---
+++ GooglePage/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GooglePage::Application.config.secret_key_base = '9086ef28612a163f048be7725ca65df2a0d5ff5a42f1e33339a31c22b8d2e368ad9700c0dd627c027b18cbf715e8702f0c955aa26ed813bf520bdc4995b52793'
